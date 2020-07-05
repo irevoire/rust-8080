d99e257f176245f91bc613f0170c69c0ad3e29fb
irevoire <irevoire@hotmail.fr>
2020-07-05 02:45:48 +0200

    move the shitty instruction from the cpu to the tests #1

--- tests/init.rs
+++ tests/init.rs
@@ -2,6 +2,14 @@ pub static mut FINISHED: bool = false;
 
 pub fn cpu(file: &str) -> rust_8080::Cpu {
     let mut cpu = rust_8080::Cpu::from_filename_at(&file, 0x100).unwrap();
+    // inject "out 1, a at 0x0000 (signal to stop the test)
+    cpu.ram[0] = 0xD3;
+    cpu.ram[1] = 0x00;
+
+    // inject "in a,0" at 0x0005 (signal to output some characters)
+    cpu.ram[5] = 0xDB;
+    cpu.ram[6] = 0x00;
+    cpu.ram[7] = 0xC9;
 
     cpu.port_in = Some(Box::new(|cpu, _| {
         let op = cpu.reg.c;
